32fa0b2e4d07bb7f5731071aa724894eaf6f55ee
coollama1 <lamemarvin@gmail.com>
2021-07-27 10:20:01 -0400

    adding project files

---
+++ src/test/java/dev/andrylat/OnlineClassroomApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.andrylat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClassroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
